5f091ab91035530c6912f9a4a78c560a8f713f21
Rodrigo Alvarenga <raalvarenga423@gmail.com>
2025-02-20 14:37:37 -0800

    Modify utils composabkle

--- src/composables/utils.js
+++ src/composables/utils.js
@@ -1,76 +1,52 @@
-import { supabase } from '@/supabase';
-import { ref, triggerRef } from 'vue';
-import { useAppStore } from '@/store/app';
+import { useCartStore } from '@/store/cart';
 import { useUserStore } from '@/store/user';
+import { useToast } from 'primevue/usetoast';
 
 export function useUtils() {
-  const salesPriceTiers = ref({});
-  const companyPriceTiers = ref({});
+  const cartStore = useCartStore();
+  const userStore = useUserStore();
+  const toast = useToast();
 
-  const getPricesTiers = async (division = 'The Outdoor Plus') => {
-    const { currentCompany } = useUserStore();
-    const { currentRole } = useUserStore();
+  const currentConfigurationName = computed(() => {
+    let baseName = '';
+    if (selectedProduct.value?.product) {
+      baseName = baseName + selectedProduct.value.product;
 
-    try {
-      const { data, error } = await supabase
-        .from('price_tiers')
-        .select('*')
-        .eq('division', division)
-        .order('name', { ascending: true });
-      if (error) throw error;
-
-      const priceTiers = {};
-      let localSalesPriceTiers = {};
-      let localCompanyPriceTiers = {};
-
-      data.forEach((tier) => {
-        if (tier.show_to_roles.includes(currentRole)) {
-          priceTiers[tier.name] = priceTiers[tier.name] || [];
-          tier.show_as =
-            tier.type === 'MAP' ||
-            tier.type === 'MSRP' ||
-            tier.type === 'Retail' ||
-            tier.type === 'RETAIL'
-              ? 'retail_price'
-              : 'account_price';
-          priceTiers[tier.name].push(tier);
-        }
-      });
-
-      if (!['ADMIN', 'TOP_SALES', 'SALES'].includes(currentRole)) {
-        let companyCost = null;
-        Object.keys(priceTiers).forEach((tierName) => {
-          if (
-            priceTiers[tierName].every((tier) =>
-              ['MAP', 'MSRP', 'Retail', 'RETAIL'].includes(tier.name),
-            )
-          ) {
-            localCompanyPriceTiers[tierName] = priceTiers[tierName];
-            delete priceTiers[tierName];
-          }
-        });
-        const flattenPriceTiers = Object.values(priceTiers).flat();
-        companyCost = flattenPriceTiers.find((tier) => tier.company === currentCompany.id);
-        if (!companyCost) {
-          companyCost = flattenPriceTiers.find((tier) => tier.default === true);
-        }
-        localCompanyPriceTiers['companyCost'] = companyCost;
-      } else {
-        localSalesPriceTiers = priceTiers;
+      if (selectedMaterial.value && selectedMaterial.value.attribute_option) {
+        baseName = baseName + ` - ${selectedMaterial.value.attribute_option}`;
       }
 
-      return {
-        salesPrices: localSalesPriceTiers,
-        companyPrices: localCompanyPriceTiers,
-      };
-    } catch (error) {
-      console.error(error);
+      Object.keys(selectedAttributes.value).forEach((key) => {
+        if (selectedAttributes.value[key] && selectedAttributes.value[key].attribute_option) {
+          baseName = baseName + ` - ${selectedAttributes.value[key].attribute_option}`;
+        }
+      });
+      return baseName;
     }
+  });
+
+  const addToCart = (getPriceBreakdown, currentConfigurationName, currentConfigurationSKU) => {
+    cartStore.addToCart(
+      getPriceBreakdown.value,
+      {
+        name: currentConfigurationName.value,
+        sku: currentConfigurationSKU.value,
+      },
+      userStore?.currentRole
+        ? userStore.currentRole
+        : userStore?.currentCompanyRole
+          ? userStore?.currentCompanyRole
+          : null,
+    );
+    toast.add({
+      severity: 'success',
+      summary: 'Added to Cart',
+      detail: 'Items have been added to your cart',
+      life: 3000,
+    });
   };
 
   return {
-    getPricesTiers,
-    salesPriceTiers,
-    companyPriceTiers,
+    addToCart,
   };
 }
