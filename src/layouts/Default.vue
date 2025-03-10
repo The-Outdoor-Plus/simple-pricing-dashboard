6b6d7cdc0bcd133d49ed03fc7e40cc214fba59e0
Rodrigo Alvarenga <raalvarenga423@gmail.com>
2025-03-10 12:12:40 -0700

    Add option to load multiple spec sheets or documents

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -8,7 +8,7 @@
     <div v-if="appStore.loading"
       class="w-full h-full min-h-screen flex justify-center items-center fixed top-0 left-0 bg-slate-600/10">
       <div class="w-full h-full flex justify-center items-center">
-        <ProgressSpinner style="width: 100px; height: 100px;" stroke-width="6" aria-label="loading"></ProgressSpinner>
+        <progressspinner style="width: 100px; height: 100px;" stroke-width="6" aria-label="loading"></progressspinner>
       </div>
     </div>
     <AtlassianWidget :key="route.fullPath" />
